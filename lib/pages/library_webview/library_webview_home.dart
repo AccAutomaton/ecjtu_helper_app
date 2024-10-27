48e87f4da76b93461253c8e565fcb112c07388b8
AcAutomaton <88975453+AccAutomaton@users.noreply.github.com>
2024-10-27 18:10:32 +0800

    Refactor: library_webview_home.dart

--- lib/pages/library_webview/library_webview_home.dart
+++ lib/pages/library_webview/library_webview_home.dart
@@ -23,15 +23,9 @@ class LibraryWebviewPage extends StatefulWidget {
 }
 
 class _LibraryWebviewPageState extends State<LibraryWebviewPage> {
-  ScanResult? _scanResult;
   late Timer _timer;
   DateTime _currentTime = DateTime.now();
   final MenuController _menuController = MenuController();
-  final ButtonStyle _menuChildrenButtonStyle = ButtonStyle(
-    backgroundColor: WidgetStateProperty.all(Colors.transparent),
-    shadowColor: WidgetStateProperty.all(Colors.transparent),
-    minimumSize: const WidgetStatePropertyAll(Size(175, 50)),
-  );
 
   @override
   void initState() {
@@ -79,77 +73,14 @@ class _LibraryWebviewPageState extends State<LibraryWebviewPage> {
                 controller: _menuController,
                 consumeOutsideTap: true,
                 menuChildren: [
-                  ElevatedButton.icon(
-                    icon: const Icon(Icons.keyboard_backspace),
-                    label: const Text("上一页"),
-                    style: _menuChildrenButtonStyle,
-                    onPressed: () {
-                      libraryWebViewController
-                          .canGoBack()
-                          .then((bool canGoBack) {
-                        if (canGoBack) {
-                          libraryWebViewController.goBack();
-                        } else {
-                          Fluttertoast.showToast(
-                              msg: "已经是第一页", gravity: ToastGravity.BOTTOM);
-                        }
-                        _menuController.close();
-                      });
-                    },
-                  ),
-                  ElevatedButton.icon(
-                    style: _menuChildrenButtonStyle,
-                    icon: const Icon(Icons.refresh),
-                    label: const Text("刷新"),
-                    onPressed: () {
-                      libraryWebViewController.reload();
-                      _menuController.close();
-                    },
-                  ),
-                  ElevatedButton.icon(
-                    style: _menuChildrenButtonStyle,
-                    icon: const Icon(Icons.delete_forever_outlined),
-                    label: const Text("清除缓存并刷新"),
-                    onPressed: () {
-                      libraryWebViewController.clearCache();
-                      libraryWebViewController.clearLocalStorage();
-                      libraryWebViewController.reload();
-                      _menuController.close();
-                    },
-                  ),
+                  buttonToPreviousPage(_menuController),
+                  buttonToRefresh(_menuController),
+                  buttonToClearRefresh(_menuController),
                 ],
               ),
               actions: [
-                IconButton(
-                  icon: const Icon(Icons.qr_code_scanner),
-                  onPressed: () {
-                    libraryWebViewController.currentUrl().then((url) async {
-                      if (url!.contains("lib2.ecjtu.edu.cn")) {
-                        _scanResult = await FlutterHmsScanKit.startScan();
-                        String? to = "http://lib2.ecjtu.edu.cn/";
-                        to = _scanResult?.value;
-                        libraryWebViewController.loadRequest(Uri.parse(to!));
-                      } else {
-                        Fluttertoast.showToast(
-                            msg: "请先登录图书馆再扫码",
-                            gravity: ToastGravity.CENTER,
-                            backgroundColor: Colors.yellow[800]);
-                      }
-                    });
-                  },
-                ),
-                IconButton(
-                  icon: const Icon(Icons.settings_outlined),
-                  onPressed: () async {
-                    await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (BuildContext context) {
-                            return const LibrarySettingsPage();
-                          }),
-                    );
-                  },
-                ),
+                buttonToScanQRCode(),
+                buttonToLibrarySettings(context),
               ],
             ),
             body: Column(
@@ -159,101 +90,181 @@ class _LibraryWebviewPageState extends State<LibraryWebviewPage> {
                   flex: 1,
                   child: Row(
                     children: [
-                      Expanded(
-                        flex: 1,
-                        child: Container(),
-                      ),
-                      ElevatedButton.icon(
-                        icon: const Icon(Icons.fast_forward_outlined),
-                        label: const Text("快速预约"),
-                        onPressed: () {
-                          // TODO
-                          Fluttertoast.showToast(
-                              msg: "敬请期待", gravity: ToastGravity.CENTER);
-                        },
-                        onLongPress: () async {
-                          // TODO
-                          Fluttertoast.showToast(
-                              msg: "敬请期待", gravity: ToastGravity.CENTER);
-                        },
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(),
-                      ),
-                      ElevatedButton.icon(
-                        icon: const Icon(Icons.access_time),
-                        label: Text(
-                            "  ${_currentTime.hour}:${_currentTime.minute.toString().padLeft(2, '0')}:${_currentTime.second.toString().padLeft(2, '0')}  "),
-                        onPressed: () {},
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(),
-                      ),
-                      ElevatedButton.icon(
-                        icon: const Icon(Icons.fact_check_outlined),
-                        label: const Text("快速签到"),
-                        onPressed: () {
-                          bool hasDefaultSeat = false;
-                          readStringData("library_has_default_room_dev_id")
-                              .then((isHasDefaultSeat) {
-                            if (isHasDefaultSeat != null) {
-                              hasDefaultSeat = bool.parse(isHasDefaultSeat);
-                              if (hasDefaultSeat) {
-                                readStringData("library_default_room_dev_id")
-                                    .then((roomDevId) {
-                                  libraryWebViewController.loadRequest(Uri.parse(
-                                      "http://update.unifound.net/wxnotice/s.aspx?c=$roomDevId"));
-                                });
-                                return;
-                              }
-                            }
-                            Fluttertoast.showToast(
-                                msg: "尚未设置默认座位，请长按该按钮进行设置。",
-                                gravity: ToastGravity.CENTER);
-                          });
-                        },
-                        onLongPress: () async {
-                          await Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (BuildContext context) {
-                              return const SettingLibraryDefaultSeatPage();
-                            }),
-                          );
-                          _menuController.close();
-                        },
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(),
-                      ),
+                      Expanded(flex: 1, child: Container()),
+                      buttonToQuickAppointment(),
+                      Expanded(flex: 1, child: Container()),
+                      timeButton(_currentTime),
+                      Expanded(flex: 1, child: Container()),
+                      buttonToQuickCheck(context),
+                      Expanded(flex: 1, child: Container()),
                     ],
                   ),
                 ),
                 Container(margin: const EdgeInsets.fromLTRB(0, 0, 0, 20)),
-                Expanded(
-                    flex: 20,
-                    child: PopScope(
-                      canPop: false,
-                      child:
-                          WebViewWidget(controller: libraryWebViewController),
-                      onPopInvoked: (didPop) {
-                        libraryWebViewController
-                            .canGoBack()
-                            .then((bool canGoBack) {
-                          if (canGoBack) {
-                            libraryWebViewController.goBack();
-                          } else {
-                            Fluttertoast.showToast(
-                                msg: "已经是第一页", gravity: ToastGravity.BOTTOM);
-                          }
-                        });
-                      },
-                    )),
+                Expanded(flex: 20, child: webViewScope()),
               ],
             )));
   }
+}
+
+ButtonStyle menuChildrenButtonStyle = ButtonStyle(
+  backgroundColor: WidgetStateProperty.all(Colors.transparent),
+  shadowColor: WidgetStateProperty.all(Colors.transparent),
+  minimumSize: const WidgetStatePropertyAll(Size(175, 50)),
+);
+
+Widget buttonToPreviousPage(MenuController menuController) {
+  return ElevatedButton.icon(
+    icon: const Icon(Icons.keyboard_backspace),
+    label: const Text("上一页"),
+    style: menuChildrenButtonStyle,
+    onPressed: () {
+      libraryWebViewController.canGoBack().then((bool canGoBack) {
+        if (canGoBack) {
+          libraryWebViewController.goBack();
+        } else {
+          Fluttertoast.showToast(msg: "已经是第一页", gravity: ToastGravity.BOTTOM);
+        }
+        menuController.close();
+      });
+    },
+  );
+}
+
+Widget buttonToRefresh(MenuController menuController) {
+  return ElevatedButton.icon(
+    style: menuChildrenButtonStyle,
+    icon: const Icon(Icons.refresh),
+    label: const Text("刷新"),
+    onPressed: () {
+      libraryWebViewController.reload();
+      menuController.close();
+    },
+  );
+}
+
+Widget buttonToClearRefresh(MenuController menuController) {
+  return ElevatedButton.icon(
+    style: menuChildrenButtonStyle,
+    icon: const Icon(Icons.delete_forever_outlined),
+    label: const Text("清除缓存并刷新"),
+    onPressed: () {
+      libraryWebViewController.clearCache();
+      libraryWebViewController.clearLocalStorage();
+      libraryWebViewController.reload();
+      menuController.close();
+    },
+  );
+}
+
+Widget buttonToScanQRCode() {
+  return IconButton(
+    icon: const Icon(Icons.qr_code_scanner),
+    onPressed: () {
+      libraryWebViewController.currentUrl().then((url) async {
+        if (url!.contains("lib2.ecjtu.edu.cn")) {
+          ScanResult? scanResult = await FlutterHmsScanKit.startScan();
+          String? to = "http://lib2.ecjtu.edu.cn/";
+          to = scanResult?.value;
+          libraryWebViewController.loadRequest(Uri.parse(to!));
+        } else {
+          Fluttertoast.showToast(
+              msg: "请先登录图书馆再扫码",
+              gravity: ToastGravity.CENTER,
+              backgroundColor: Colors.yellow[800]);
+        }
+      });
+    },
+  );
+}
+
+Widget buttonToLibrarySettings(BuildContext context) {
+  return IconButton(
+    icon: const Icon(Icons.settings_outlined),
+    onPressed: () async {
+      await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (BuildContext context) {
+          return const LibrarySettingsPage();
+        }),
+      );
+    },
+  );
+}
+
+Widget buttonToQuickAppointment() {
+  return ElevatedButton.icon(
+    icon: const Icon(Icons.fast_forward_outlined),
+    label: const Text("快速预约"),
+    onPressed: () {
+      // TODO
+      Fluttertoast.showToast(msg: "敬请期待", gravity: ToastGravity.CENTER);
+    },
+    onLongPress: () async {
+      // TODO
+      Fluttertoast.showToast(msg: "敬请期待", gravity: ToastGravity.CENTER);
+    },
+  );
+}
+
+Widget timeButton(DateTime currentTime) {
+  return ElevatedButton.icon(
+    icon: const Icon(Icons.access_time),
+    label: Text(
+        "  ${currentTime.hour}:${currentTime.minute.toString().padLeft(2, '0')}:${currentTime.second.toString().padLeft(2, '0')}  "),
+    onPressed: () {},
+  );
+}
+
+Widget buttonToQuickCheck(
+  BuildContext context,
+) {
+  return ElevatedButton.icon(
+    icon: const Icon(Icons.fact_check_outlined),
+    label: const Text("快速签到"),
+    onPressed: () {
+      bool hasDefaultSeat = false;
+      readStringData("library_has_default_room_dev_id")
+          .then((isHasDefaultSeat) {
+        if (isHasDefaultSeat != null) {
+          hasDefaultSeat = bool.parse(isHasDefaultSeat);
+          if (hasDefaultSeat) {
+            readStringData("library_default_room_dev_id").then((roomDevId) {
+              libraryWebViewController.loadRequest(Uri.parse(
+                  "http://update.unifound.net/wxnotice/s.aspx?c=$roomDevId"));
+            });
+            return;
+          }
+        }
+        Fluttertoast.showToast(
+            msg: "尚未设置默认座位，请长按该按钮进行设置。", gravity: ToastGravity.CENTER);
+      });
+    },
+    onLongPress: () async {
+      await Navigator.push(
+        context,
+        MaterialPageRoute(builder: (BuildContext context) {
+          return const SettingLibraryDefaultSeatPage();
+        }),
+      );
+    },
+  );
+}
+
+Widget webViewScope() {
+  return PopScope(
+    canPop: false,
+    child: WebViewWidget(controller: libraryWebViewController),
+    onPopInvoked: (didPop) {
+      libraryWebViewController.canGoBack().then((bool canGoBack) {
+        if (canGoBack) {
+          libraryWebViewController.goBack();
+        } else {
+          Fluttertoast.showToast(msg: "已经是第一页", gravity: ToastGravity.BOTTOM);
+        }
+      });
+    },
+  );
 }
 
 final WebViewController libraryWebViewController = WebViewController()
